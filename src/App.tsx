46319f2f105588527607c4d3c910d1aaf43784be
caldric <cvaldric@gmail.com>
2021-01-13 18:10:21 -0500

    Add /register route to manually test Register component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Register from './Register';
 
 const App = () => {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
