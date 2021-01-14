91b8b96c3f8f48cc59c231944ca9131e32aff39a
caldric <cvaldric@gmail.com>
2021-01-14 00:28:53 -0500

    Refactor: use .toHaveLength method for relevant tests

--- src/Register.test.tsx
+++ src/Register.test.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
 import Register from './Register';
 
-const setup = () => shallow(<Register />);
-
 let wrapper: ShallowWrapper;
 beforeEach(() => {
-  wrapper = setup();
+  wrapper = shallow(<Register />);
 });
 
 test('Renders without errors', () => {
   const appComponent = wrapper.find('#Register');
-  expect(appComponent.length).toBe(1);
+  expect(appComponent).toHaveLength(1);
 });
 
 test('Displays text to sign up for new account', () => {
@@ -27,7 +25,7 @@ describe('Email address text input', () => {
   });
 
   test('Renders without errors', () => {
-    expect(emailInput.length).toBe(1);
+    expect(emailInput).toHaveLength(1);
   });
 
   test('Has the "email" input type', () => {
