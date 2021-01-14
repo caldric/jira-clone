05d5808ad1dff1de1e150ec9af1ad0daa14f3a3f
caldric <cvaldric@gmail.com>
2021-01-14 00:35:14 -0500

    Red: password input renders without errors

--- src/Register.test.tsx
+++ src/Register.test.tsx
@@ -17,40 +17,45 @@ test('Displays text to sign up for new account', () => {
   expect(signupHeader.text().length).toBeGreaterThan(0);
 });
 
-describe('Email address text input', () => {
-  let emailInput: ShallowWrapper;
+describe('User registration form', () => {
+  describe('Email address text input', () => {
+    let emailInput: ShallowWrapper;
 
-  beforeEach(() => {
-    emailInput = wrapper.find('#email');
+    beforeEach(() => {
+      emailInput = wrapper.find('#email');
+    });
+
+    test('Renders without errors', () => {
+      expect(emailInput).toHaveLength(1);
+    });
+
+    test('Has the "email" input type', () => {
+      expect(emailInput.prop('type')).toBe('email');
+    });
+
+    test('User is able to type an email in the input', () => {
+      // Create sample email
+      const email: string = 'user@email.com';
+
+      // Simulate user typing an email
+      emailInput.simulate('change', { target: { value: email } });
+
+      // Input text value should be equal to the email
+      const inputText = wrapper.find('#email').prop('value');
+      expect(inputText).toBe(email);
+    });
   });
 
-  test('Renders without errors', () => {
-    expect(emailInput).toHaveLength(1);
+  describe('Password text input', () => {
+    test('Renders without errors', () => {
+      const passwordInput = wrapper.find('#password');
+      expect(passwordInput).toHaveLength(1);
+    });
   });
 
-  test('Has the "email" input type', () => {
-    expect(emailInput.prop('type')).toBe('email');
+  describe('Sign up button', () => {
+    test('Renders without errors', () => {});
   });
-
-  test('User is able to type an email in the input', () => {
-    // Create sample email
-    const email: string = 'user@email.com';
-
-    // Simulate user typing an email
-    emailInput.simulate('change', { target: { value: email } });
-
-    // Input text value should be equal to the email
-    const inputText = wrapper.find('#email').prop('value');
-    expect(inputText).toBe(email);
-  });
-});
-
-describe('Password text input', () => {
-  test('Renders without errors', () => {});
-});
-
-describe('Sign up button', () => {
-  test('Renders without errors', () => {});
 });
 
 describe('Log in link', () => {
